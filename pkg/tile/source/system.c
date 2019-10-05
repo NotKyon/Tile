2de344e7e64082427a9ffe6fc8d8a7bc0468dca8
NotKyon <nocannedmeat@gmail.com>
2019-10-05 08:55:41 -0500

    Fix time queries.

--- pkg/tile/source/system.c
+++ pkg/tile/source/system.c
@@ -1,17 +1,43 @@
 #include <tile/system.h>
 
-#if defined( _WIN32 )
-static TlU64 tlSys_TimerFreq( void )
-{
-	LARGE_INTEGER f;
+#define TL_TIME_NANOSECS  1000000000
+#define TL_TIME_MICROSECS 1000000
+#define TL_TIME_MILLISECS 1000
 
-	if( !QueryPerformanceFrequency( &f ) ) {
-		f.QuadPart = 1;
+#define TL_TIME_ConvRes(T_,F_,R_) \
+	(((TlU64)(T_))*((TlU64)(R_))/((TlU64)(F_)))
+
+/* Convert a time (t) from the system frequency to the target frequency (tf) */
+static TlU64 tlConvFreq( TlU64 t, TlU64 tf )
+{
+#if defined(_WIN32)
+	static LARGE_INTEGER f;
+	static int didinit = 0;
+
+	if( !didinit ) {
+		if( !QueryPerformanceFrequency( &f ) ) {
+			return 0;
+		}
+		didinit = 1;
 	}
 
-	return ( TlU64 )f.QuadPart;
+	return TL_TIME_ConvRes( t, f.QuadPart, tf );
+#elif defined(__APPLE__)
+	static TlU64 rate = 0;
+
+	if( __builtin_expect( rate == 0, 0 ) ) {
+		static mach_timebase_info_data_t info;
+		if( mach_timebase_info( &info ) != KERN_SUCCESS ) {
+			return 0;
+		}
+		rate = ((TlU64)1000000000)*info.numer/info.denom;
+	}
+
+	return TL_TIME_ConvRes( t, rate, tf );
+# else
+	return TL_TIME_ConvRes( t, TL_TIME_NANOSECS, tf );
+# endif
 }
-#endif
 
 #ifdef CLOCK_MONOTONIC_PRECISE
 # define TILE_POSIX_CLOCK CLOCK_MONOTONIC_PRECISE
@@ -25,32 +51,31 @@ static TlU64 tlSys_TimerFreq( void )
 # define TILE_POSIX_CLOCK CLOCK_REALTIME
 #endif
 
-TlU64 tlSys_Microtime( void )
+static TlU64 tlQueryTime( void )
 {
-#if defined( _WIN32 )
-	static TlU64 base = 0;
+# if defined(_WIN32)
 	LARGE_INTEGER t;
-	TlU64 r;
 
-	t.QuadPart = 0;
-	QueryPerformanceCounter( ( LARGE_INTEGER * )&t );
-
-	r = ( t.QuadPart - base )*1000000/tlSys_TimerFreq();
-	if( TL_UNLIKELY( !base ) ) {
-		base = t.QuadPart;
-		r = 0;
+	if( !QueryPerformanceCounter( &t ) ) {
+		return 0;
 	}
 
-	return r;
-#elif defined( __linux__ ) || defined( __linux ) || defined( linux )
-# ifdef TILE_POSIX_CLOCK
+	return t.QuadPart;
+# elif defined(__APPLE__)
+	static TlU64 base = 0;
+	if( __builtin_expect( base == 0, 0 ) ) {
+		base = mach_absolute_time();
+	}
+	return mach_absolute_time() - base;
+# else
 	struct timespec t;
 
+#  if TILE_POSIX_CLOCK != -1
 	if( clock_gettime( TILE_POSIX_CLOCK, &t ) == 0 ) {
-		return ((TlU64)t.tv_sec)*1000000 + (TlU64)(t.tv_nsec/1000);
+		return (((TlU64)t.tv_sec)*AXTIME_NANOSECS) + (TlU64)t.tv_nsec;
 	}
 	else
-# endif /*TILE_POSIX_CLOCK*/
+#  endif /*TILE_POSIX_CLOCK != -1*/
 	{
 		struct timeval tv;
 
@@ -58,25 +83,12 @@ TlU64 tlSys_Microtime( void )
 			return 0;
 		}
 
-		return ((TlU64)tv.tv_sec)*1000000 + (TlU64)tv.tv_usec;
+		return (((TlU64)tv.tv_sec)*AXTIME_NANOSECS) +
+			(TlU64)tv.tv_usec*1000;
 	}
-#elif defined( __APPLE__ )
-	/* THIS IS NOT TESTED */
-	static mach_timebase_info_data_t info;
-	static int didinit = 0;
-
-	TlU64 v;
-
-	if( !didinit ) {
-		if( mach_timebase_info( &info ) != KERN_SUCCESS ) {
-			return 0;
-		}
-		didinit = 1;
-	}
-
-	v = mach_absolute_time();
-	return v*info.numer/(((TlU64)info.denom)*1000000);
-#else
-# error Unknown or unsupported platform.
-#endif
+# endif
+}
+TlU64 tlSys_Microtime( void )
+{
+	return tlConvFreq( tlQueryTime(), TL_TIME_MICROSECS );
 }
